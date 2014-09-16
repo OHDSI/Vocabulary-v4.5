a7a37b6a6f5c0d43038d290ab1f119adc3b4c0c4
Christian Reich <reich@ohdsi.org>
2014-09-16 14:20:32 -0400

    fixed dump.pl
    Fixed source_to_concept_map to filter only by source_concept_id. That
    isn't quite correct, but we will have to live with it for now.

--- dump.pl
+++ dump.pl
@@ -1,66 +1,80 @@
 #!/usr/bin/perl
-
 use warnings;
 use strict;
 use DBI;
 use POSIX;
 use Archive::Zip;
-
 sub table_cols_query { 'SELECT COLUMN_NAME FROM ALL_TAB_COLUMNS WHERE OWNER = ? AND TABLE_NAME = ? ORDER BY COLUMN_ID' }
-
 sub table_cols { [ map { $_->[0] } @{ shift->selectall_arrayref(table_cols_query, undef, uc shift, uc shift) } ] }
-
 sub csv_line { map { "$_\n" } join ',', map { $_ //= ''; s/[\",]/\\$&/g; $_ } map { @$_ } @_ }
-
 sub all_vocabularies { map { $_->[0] } @{ shift->selectall_arrayref('SELECT * FROM VOCABULARY') } }
 
-sub csv_dump { # (dabatase handler, name of table)
-    my ($dbh, $table) = @_;
-    my $cols = table_cols $dbh, $dbh->{Username}, $table->{name};
-    my $sth = $dbh->prepare($table->{query});
-    $sth->execute(@{$table->{params}});
-    my $dump = tmpnam;
-    open my $fh, '>', $dump;
-    print $fh csv_line($cols);
-    while (my $line = $sth->fetch) {
-		print $fh csv_line($line);
+sub csv_dump { # (dabatase handler, name of table
+	my ($dbh, $table) = @_;
+	my $cols = table_cols $dbh, $dbh->{Username}, $table->{name};
+	my $sth = $dbh->prepare(join(' ', sprintf('SELECT %s FROM %s t', join(',', @$cols), $table->{name}), $table->{query}));
+	$sth->execute(@{$table->{params}});
+	my $dump = tmpnam;
+	open my $fh, '>', $dump;
+	print $fh csv_line $cols;
+	while (my $line = $sth->fetch) {
+		print $fh csv_line $line;
 	}
-    close $fh;
-    return $dump;
+	close $fh;
+	return $dump;
 }
 
 do { print <<END
-Usage:
-./dump.pl \<user/pass\@host/sid\> \<output.zip\> [vocabulary ids]
-'vocabulary ids' is a comma-separated list of required vocabularies, omit the list in order to dump all available 
+	Usage:
+	./dump.pl \<user/pass\@host/sid\> \<output.zip\> [vocabulary ids]
+	'vocabulary ids' is a comma-separated list of required vocabularies, omit the list in order to dump all available
 END
 } and exit unless @ARGV;
 
-die unless shift =~ /^(.+)\/(.+)\@(.+)\/(.+)$/;
-my $dbh = DBI->connect(sprintf('dbi:Oracle:host=%s;sid=%s', $3, $4), $1, $2) or die("Valid database access information required");
-my $output = shift or die("Output file name required");
+die "Valid database access information required." unless shift =~ /^(.+)\/(.+)\@(.+)\/(.+)$/;
+my $dbh = DBI->connect(sprintf('dbi:Oracle:host=%s;sid=%s', $3, $4), $1, $2) or die "Valid database access information required.";
+$dbh->do('ALTER SESSION SET NLS_DATE_FORMAT="YYYYMMDD"');
+my $output = shift or die "File name for output file required.";
 my @vocabularies = split /,/, (shift or join ',', all_vocabularies $dbh);
 my $placeholder = join ', ', split //, '?' x @vocabularies;
-
 my $zip = new Archive::Zip;
-do {
-    my $dump = csv_dump $dbh, $_;
-    $zip->addFile($dump, sprintf('%s.csv', $_->{name}));
-} for
-    {
+warn "Writing vocabularies ".join(',', @vocabularies)." to $output.";
+
+$zip->addFile(csv_dump($dbh, $_), sprintf('%s.csv', $_->{name})) for
+{
 	name => 'CONCEPT',
-    	query => sprintf('SELECT * FROM CONCEPT WHERE VOCABULARY_ID IN (%s)', $placeholder),
+	query => sprintf('WHERE VOCABULARY_ID IN (%s)', $placeholder),
 	params => [ @vocabularies ],
-    },
-    {
+},
+{
 	name => 'CONCEPT_RELATIONSHIP',
-	query => sprintf('SELECT * FROM CONCEPT_RELATIONSHIP WHERE EXISTS (SELECT * FROM CONCEPT WHERE CONCEPT_ID_1 = CONCEPT_ID AND VOCABULARY_ID IN (%s)) AND EXISTS (SELECT * FROM CONCEPT WHERE CONCEPT_ID_2 = CONCEPT_ID AND VOCABULARY_ID IN (%s))', $placeholder, $placeholder),
+	query => sprintf('WHERE EXISTS (SELECT * FROM CONCEPT WHERE CONCEPT_ID_1 = CONCEPT_ID AND VOCABULARY_ID IN (%s)) AND EXISTS (SELECT * FROM CONCEPT WHERE CONCEPT_ID_2 = CONCEPT_ID AND VOCABULARY_ID IN (%s))', $placeholder, $placeholder),
 	params => [ @vocabularies, @vocabularies ],
-    },
-    {
+},
+{
 	name => 'SOURCE_TO_CONCEPT_MAP',
-	query => sprintf('SELECT * FROM SOURCE_TO_CONCEPT_MAP WHERE SOURCE_VOCABULARY_ID IN (%s) AND TARGET_VOCABULARY_ID IN (%s)', $placeholder, $placeholder),
-	params => [ @vocabularies, @vocabularies ],
-    };
-die unless $zip->writeToFileNamed($output) == Archive::Zip::AZ_OK;
+	query => sprintf('WHERE SOURCE_VOCABULARY_ID IN (%s)', $placeholder),
+	params => [ @vocabularies ],
+},
+{
+	name => 'CONCEPT_ANCESTOR',
+	query => sprintf('WHERE EXISTS (SELECT * FROM CONCEPT WHERE ANCESTOR_CONCEPT_ID = CONCEPT_ID AND VOCABULARY_ID IN (%s)) AND EXISTS (SELECT * FROM CONCEPT WHERE DESCENDANT_CONCEPT_ID = CONCEPT_ID AND VOCABULARY_ID IN (%s))', $placeholder, $placeholder),
+	params => [ @vocabularies, @vocabularies ],	
+},
+{
+	name => 'CONCEPT_SYNONYM',
+	query => sprintf('WHERE EXISTS (SELECT * FROM CONCEPT c WHERE t.CONCEPT_ID = c.CONCEPT_ID AND VOCABULARY_ID IN (%s))', $placeholder),
+	params => [ @vocabularies ],	
+},
+{
+	name => 'VOCABULARY',
+	query => sprintf('WHERE VOCABULARY_ID IN (%s)', $placeholder),
+	params => [ @vocabularies ],
+},
+{
+	name => 'RELATIONSHIP',
+	query => '',
+	params => [],
+};
+die "Cannot write zip file: $!" unless $zip->writeToFileNamed($output) == Archive::Zip::AZ_OK;
 unlink $_->{externalFileName} for $zip->members;
