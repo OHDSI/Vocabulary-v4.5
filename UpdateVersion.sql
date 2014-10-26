ef1c2103ac7aa135ceda292151b54be12509a213
Christian Reich <reich@ohdsi.org>
2014-10-26 18:20:33 -0400

    Versioning

---
+++ UpdateVersion.sql
@@ -0,0 +1,2 @@
+update vocabulary set vocabulary_name='OMOP Vocabulary v4.5 20-Oct-2014' where vocabulary_id=0;
+commit;
---
+++ version.sql
@@ -0,0 +1,1 @@
+update vocabulary set vocabulary_name='OMOP Vocabulary v4.5 15-Oct-2014' where vocabulary_id=0;
